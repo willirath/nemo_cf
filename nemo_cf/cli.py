d0f0905b4894143ef3ea817ffd83b7f94680bc0c
Willi Rath <wrath@geomar.de>
2020-02-05 13:28:15 +0100

    Add cli tool for downloading example data

--- nemo_cf/cli.py
+++ nemo_cf/cli.py
@@ -1,14 +1,26 @@
 """Console script for nemo_cf."""
 import sys
+import zipfile
+import urllib.request
 import click
 
 
 @click.command()
-def main(args=None):
-    """Console script for nemo_cf."""
-    click.echo("Replace this message by putting your code into "
-               "nemo_cf.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
+@click.argument('target-dir')
+@click.option(
+    '--url',
+    default=(
+        'https://zenodo.org/record/3634491/files/'
+        'NEMO_GYRE_test_data_all_files.v2020.02.03.1.zip'
+    )
+)
+def main(target_dir, url):
+    """Download NEMO example data."""
+
+    filehandle = urllib.request.urlretrieve(url)[0]
+    zip_file_object = zipfile.ZipFile(filehandle, 'r')
+    zip_file_object.extractall(target_dir)
+
     return 0
 
 
---
+++ tests/test_nemo_cf_cli.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""Tests for the `nemo_cf.cli` package."""
+
+from nemo_cf import cli
+
+
+def test_command_line_interface():
+    """Test the CLI."""
+    runner = CliRunner()
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert 'nemo_cf.cli.main' in result.output
+    help_result = runner.invoke(cli.main, ['--help'])
+    assert help_result.exit_code == 0
+    assert '--help  Show this message and exit.' in help_result.output
