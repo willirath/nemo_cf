1f1eb7c230c596e453cf2ce08dcc426de8cff448
Willi Rath <wrath@geomar.de>
2020-02-06 17:06:09 +0100

    Test for correct help message

--- tests/test_nemo_cf_cli.py
+++ tests/test_nemo_cf_cli.py
@@ -12,4 +12,4 @@ def test_command_line_interface():
     runner = CliRunner()
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert 'Download NEMO example data.' in help_result.output
