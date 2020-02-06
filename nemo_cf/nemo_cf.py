286feb4c5d190ef664aeed202f9c71424785850e
Willi Rath <willirath@users.noreply.github.com>
2020-02-06 18:38:48 +0100

    Add vars attribute updater (#3)
    * Add vars attr updater
    
    * Restore CF attribute dicts
    
    * Ignore FS metadata
    
    * Add label dropper
    
    * Ensure xarray is present
    
    * Add test for label dropper
    
    * Fix ds creation
    
    * Fix tests
    
    * Fix function name
    
    * Drop 3.5 support

--- nemo_cf/nemo_cf.py
+++ nemo_cf/nemo_cf.py
@@ -1,1 +1,21 @@
 """Main module."""
+
+from .cf_attributes import mesh_mask_attrs
+
+
+def update_all_vars_attrs(dataset, attrs=None):
+    """Update attributes of all vars."""
+    for varname, new_attrs in attrs.items():
+        dataset[varname].attrs.update(new_attrs)
+    return dataset
+
+
+def update_mesh_mask_attrs(dataset, attrs=mesh_mask_attrs):
+    """Update a mesh_mask dataset to have CF compliant metadata."""
+    return update_all_vars_attrs(dataset, attrs=attrs)
+
+
+def safely_drop_vars(dataset, vars=None):
+    """Drop labels dataset if they are present."""
+    valid_vars = filter(lambda var: var in dataset, vars)
+    return dataset.drop_vars(valid_vars)
---
+++ tests/test_nemo_cf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""Tests for the `nemo_cf.cli` package."""
+
+from nemo_cf.nemo_cf import update_all_vars_attrs, safely_drop_vars
+
+import xarray as xr
+
+
+def test_update_all_vars_attrs_works():
+
+    dataset = xr.DataArray([], name="var_01").to_dataset()
+    new_attrs = {
+        "var_01": {"units": "meters", "coordinates": ""}
+    }
+
+    dataset = update_all_vars_attrs(dataset, attrs=new_attrs)
+
+    assert dataset["var_01"].attrs["units"] == "meters"
+    assert dataset["var_01"].attrs["coordinates"] == ""
+
+
+def test_safe_var_dropper():
+    dataset = xr.DataArray([], name="var_01").to_dataset()
+
+    assert "var_01" in safely_drop_vars(dataset, ["var_02", "var_03"])
+
+    assert "var_01" not in safely_drop_vars(dataset, ["var_01", "var_02"])
