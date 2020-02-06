d76765df575d4388f8a5b1e30507e365c6b7b7fb
Willi Rath <wrath@geomar.de>
2020-02-06 16:59:44 +0100

    Skip test call without default args

--- tests/test_nemo_cf_cli.py
+++ tests/test_nemo_cf_cli.py
@@ -2,7 +2,7 @@
 
 """Tests for the `nemo_cf.cli` package."""
 
-from click.testing import CliRunner	
+from click.testing import CliRunner
 
 from nemo_cf import cli
 
@@ -10,9 +10,6 @@ from nemo_cf import cli
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'nemo_cf.cli.main' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
