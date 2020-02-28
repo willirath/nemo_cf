6eaf2cdcb595546880a3283a11ef118d42fccade
Willi Rath <wrath@geomar.de>
2020-02-28 12:46:49 +0100

    Separate nemo_cf cli

--- setup.py
+++ setup.py
@@ -4,49 +4,56 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.md') as history_file:
+with open("HISTORY.md") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = [
+    "Click>=7.0",
+]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Willi Rath",
-    author_email='wrath@geomar.de',
-    python_requires='>=3.6',
+    author_email="wrath@geomar.de",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Make NEMO output CF compliant",
     entry_points={
-        'console_scripts': [
-            'nemo_cf=nemo_cf.cli:main',
+        "console_scripts": [
+            "nemo_cf_info=nemo_cf.cli:print_info",
+            "nemo_cf_download_data=nemo_cf.cli:download_data",
         ],
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='nemo_cf',
-    name='nemo_cf',
-    packages=find_packages(include=['nemo_cf', 'nemo_cf.*']),
+    keywords="nemo_cf",
+    name="nemo_cf",
+    packages=find_packages(include=["nemo_cf", "nemo_cf.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/willirath/nemo_cf',
-    version='0.1.0',
+    url="https://github.com/willirath/nemo_cf",
+    version="0.1.0",
     zip_safe=False,
 )
