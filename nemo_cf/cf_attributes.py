399d4a440ef0019059119840aee6cf540f8d929c
Willi Rath <wrath@geomar.de>
2020-02-28 12:25:17 +0100

    Adopt salish sea project attributes

--- nemo_cf/cf_attributes.py
+++ nemo_cf/cf_attributes.py
@@ -6,9 +6,9 @@ mesh_mask_attrs = {}
 mesh_mask_attrs.update(
     {
         f"glam{grid}": {
-            "long_name": "longitude",
+            "long_name": f"longitude of {grid.upper()}-grid points",
             "units": "degrees_east",
-            "standard_name": "longitude"
+            "standard_name": "longitude",
         }
         for grid in ["t", "u", "v", "f"]
     }
@@ -18,9 +18,9 @@ mesh_mask_attrs.update(
 mesh_mask_attrs.update(
     {
         f"gphi{grid}": {
-            "long_name": "latitude",
+            "long_name": f"latitude of {grid.upper()}-grid points",
             "units": "degrees_north",
-            "standard_name": "latitude"
+            "standard_name": "latitude",
         }
         for grid in ["t", "u", "v", "f"]
     }
@@ -30,10 +30,10 @@ mesh_mask_attrs.update(
 mesh_mask_attrs.update(
     {
         f"gdep{grid}_1d": {
-            "long_name": "depth",
+            "long_name": f"depth of {grid.upper()}-grid points",
             "units": "meters",
             "positive": "down",
-            "standard_name": "depth"
+            "standard_name": "depth",
         }
         for grid in ["t", "w"]
     }
@@ -43,9 +43,9 @@ mesh_mask_attrs.update(
 mesh_mask_attrs.update(
     {
         f"e1{grid}": {
-            "long_name": "zonal grid constant",
+            "long_name": f"grid spacing on {grid.upper()}-grid in u direction",
             "units": "meters",
-            "coordinates": f"glam{grid} gphi{grid}"
+            "coordinates": f"glam{grid} gphi{grid}",
         }
         for grid in ["t", "u", "v", "f"]
     }
@@ -55,9 +55,9 @@ mesh_mask_attrs.update(
 mesh_mask_attrs.update(
     {
         f"e2{grid}": {
-            "long_name": "meridional grid constant",
+            "long_name": f"grid spacing on {grid.upper()}-grid in v direction",
             "units": "meters",
-            "coordinates": f"glam{grid} gphi{grid}"
+            "coordinates": f"glam{grid} gphi{grid}",
         }
         for grid in ["t", "u", "v", "f"]
     }
@@ -67,9 +67,9 @@ mesh_mask_attrs.update(
 mesh_mask_attrs.update(
     {
         f"e3{grid}_1d": {
-            "long_name": "vertical grid constant",
+            "long_name": f"grid spacing on {grid.upper()}-grid in w direction",
             "units": "meters",
-            "coordinates": f"gdep{grid}_1d"
+            "coordinates": f"gdep{grid}_1d",
         }
         for grid in ["t", "w"]
     }
@@ -81,9 +81,10 @@ mesh_mask_attrs.update(
 mesh_mask_attrs.update(
     {
         f"{grid}mask": {
-            "long_name": "land point mask",
-            "units": "boolean",
-            "coordinates": f"gdept_1d glam{grid} gphi{grid}"
+            "long_name": f"mask for {grid.upper()}-grid",
+            "flag_meanings": "land, water",
+            "flag_values": "0, 1",
+            "coordinates": f"gdept_1d glam{grid} gphi{grid}",
         }
         for grid in ["t", "u", "v", "f"]
     }
@@ -93,18 +94,32 @@ mesh_mask_attrs.update(
 mesh_mask_attrs.update(
     {
         f"{grid}maskutil": {
-            "long_name": "land point mask",
-            "units": "boolean",
-            "coordinates": f"glam{grid} gphi{grid}"
+            "long_name": f"mask for {grid.upper()}-grid without halo",
+            "flag_meanings": "land, water",
+            "flag_values": "0, 1",
+            "coordinates": f"glam{grid} gphi{grid}",
         }
         for grid in ["t", "u", "v", "f"]
     }
 )
 
-# number of wet grid points
-mesh_mask_attrs.update({
-    "mbathy": {
-        "long_name": "number of ocean levels at xy grid point",
-        "coordinates": "glamt gphit"
+# coriolis paramter
+mesh_mask_attrs.update(
+    {
+        "ff": {
+            "long_name": "Coriolis parameter on F-grid",
+            "units": "s-1",
+            "coordinates": "glamf gphif",
+        }
     }
-})
+)
+
+# number of wet grid points
+mesh_mask_attrs.update(
+    {
+        "mbathy": {
+            "long_name": "number of ocean levels at xy grid point",
+            "coordinates": "glamt gphit",
+        }
+    }
+)
